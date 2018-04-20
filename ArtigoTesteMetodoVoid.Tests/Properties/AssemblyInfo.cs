77be5980dee9af61a8625ffbeae8560cf8961869
Danilo Oliveira <danilo.t0458.tirrell@xpi.com.br>
2018-04-20 15:53:26 -0300

    Commit do projeto

---
+++ ArtigoTesteMetodoVoid.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ArtigoTesteMetodoVoid.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ArtigoTesteMetodoVoid.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("10dee72f-dc66-4197-b294-e46c561c1396")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ArtigoTesteMetodoVoid.Tests/UsuarioTestes.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using ArtigoTesteMetodoVoid.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ArtigoTesteMetodoVoid.Tests
+{
+    [TestClass]
+    public class UsuarioTestes
+    {
+        private static Usuario _usuario;
+        public UsuarioTestes() => _usuario = new Usuario();
+
+        [TestInitialize]
+        public void Configuracao()
+        {
+            _usuario.Id = Guid.NewGuid();
+            _usuario.Nome = "Nome do Usuario Teste";
+        }
+
+        [TestMethod]
+        public void Inserir_Usuario_Com_Sucesso()
+        {
+            var resultado = _usuario.InserirUsuario(_usuario);
+
+            Assert.IsNotNull(resultado.Id);
+            Assert.AreNotEqual(Guid.Empty, resultado.Id);
+        }
+
+        [TestMethod]
+        public void Teste_Para_Inativar_Usuario()
+        {
+            //
+        }
+
+        [TestCleanup]
+        public void LimparTestes()
+        {
+            _usuario.DeletarUsuario(_usuario.Id);
+        }
+    }
+}
